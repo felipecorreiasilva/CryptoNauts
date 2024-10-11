04d342e1528e2018e8c2f4f95992830ade79c63a
felipecorreiasilva <felipecorreiasilva@outlook.com>
2024-10-11 05:27:56 -0300

    next formatting

--- cryptonauts/tailwind.config.ts
+++ cryptonauts/tailwind.config.ts
@@ -11,6 +11,9 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: {
+          900: "#0f143b",
+        },
       },
     },
   },
