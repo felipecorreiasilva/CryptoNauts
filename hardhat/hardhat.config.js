92320d8c450916fc1e133b04c6376aab34a89d4a
felipecorreiasilva <felipecorreiasilva@outlook.com>
2024-10-14 19:27:38 -0300

    adding setting and allowUnlimitedContractSize

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -3,10 +3,18 @@ require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.27",
+  settings: {
+    optimizer: {
+      enabled: true,
+      runs: 1000,
+    },
+  },
+  allowUnlimitedContractSize: true,
   networks: {
     opsepolia: {
       url: process.env.ALCHEMY_TESTNET_PROVIDER_URL,
       accounts: [`0x${process.env.MM_PRIVATE_KEY}`]
-    }
+    },
+    
   }
 };
