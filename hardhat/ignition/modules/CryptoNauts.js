b4f59305591d2c366e02cab868074db110e21c81
felipecorreiasilva <felipecorreiasilva@outlook.com>
2024-10-16 16:42:30 -0300

    adding addresses in parameter to deploy

--- hardhat/ignition/modules/CryptoNauts.js
+++ hardhat/ignition/modules/CryptoNauts.js
@@ -5,7 +5,7 @@ const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 
 module.exports = buildModule("CryptoNautsModule", (m) => {
   
-  const cryptoNauts = m.contract("CryptoNauts", []);
+  const cryptoNauts = m.contract("CryptoNauts", ['0x26fF200166F0a163c89a1C9616e98D97AF3164b7']);
 
   return { cryptoNauts };
 });
--- hardhat/ignition/modules/CryptoNautsSale.js
+++ hardhat/ignition/modules/CryptoNautsSale.js
@@ -5,7 +5,7 @@ const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 
 module.exports = buildModule("CryptoNautsSaleModule", (m) => {
   
-  const cryptoNautsSale = m.contract("CryptoNautsSale", []);
+  const cryptoNautsSale = m.contract("CryptoNautsSale", ['0x26fF200166F0a163c89a1C9616e98D97AF3164b7','0x61Ec26aA57019C486B10502285c5A3D4A4750AD7']);
 
   return { cryptoNautsSale };
 });
