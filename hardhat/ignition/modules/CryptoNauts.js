3cd274da1f868ebf0a2324a1b60f524481847ab8
felipecorreiasilva <felipecorreiasilva@outlook.com>
2024-10-18 15:35:06 -0300

    deploy contract CryptoNautsSale

--- hardhat/ignition/modules/CryptoNauts.js
+++ hardhat/ignition/modules/CryptoNauts.js
@@ -5,7 +5,7 @@ const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 
 module.exports = buildModule("CryptoNautsModule", (m) => {
   
-  const cryptoNauts = m.contract("CryptoNauts", []);
+  const cryptoNauts = m.contract("CryptoNauts", ['0x7B51fb7615d12221447B57BC6d268847153A7461']);
 
   return { cryptoNauts };
 });
--- hardhat/ignition/modules/CryptoNautsSale.js
+++ hardhat/ignition/modules/CryptoNautsSale.js
@@ -5,7 +5,7 @@ const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 
 module.exports = buildModule("CryptoNautsSaleModule", (m) => {
   
-  const cryptoNautsSale = m.contract("CryptoNautsSale", []);
+  const cryptoNautsSale = m.contract("CryptoNautsSale", ['0x7B51fb7615d12221447B57BC6d268847153A7461','0x61Ec26aA57019C486B10502285c5A3D4A4750AD7']);
 
   return { cryptoNautsSale };
 });
